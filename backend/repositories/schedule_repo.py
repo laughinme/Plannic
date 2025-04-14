97c7e91c7fb0ed8996637f01f7135e2e2a17d825
Mark <wixw1cky7@gmail.com>
2025-04-15 01:56:18 +0700

    make 3 main schedule endpoints work

--- backend/repositories/schedule_repo.py
+++ backend/repositories/schedule_repo.py
@@ -9,9 +9,7 @@ class ScheduleRepo:
     def __init__(self, session: AsyncSession):
         self.session = session
     
-    
-    async def get_day_lessons(self, class_id: str, day: WeekDay):
-        print(day.name)
+    def basic_selection(self):
         query = (
             select(Schedule)
             .options(
@@ -21,14 +19,39 @@ class ScheduleRepo:
                 selectinload(Schedule.group),
                 selectinload(Schedule.lesson_time),
             )
+        )
+        return query
+    
+    async def get_day_lessons(self, class_id: str, day: WeekDay) -> list[Schedule]:
+        query = (
+            self.basic_selection()
             .where(
                 Schedule.day == day.name,
                 Schedule.class_id == class_id,
             )
+            .order_by(Schedule.lesson_number)
         )
-        day_schedule = await self.session.scalars(query)
-        day_schedule = day_schedule.all()
-        print(day_schedule)
+        result = await self.session.scalars(query)
+        day_schedule = result.all()
         return day_schedule
-        
-        
+
+
+    async def get_week_lessons(self, class_id: str) -> list[Schedule]:
+        query = (
+            self.basic_selection()
+            .where(Schedule.class_id == class_id)
+            .order_by(Schedule.day)
+        )
+        result = await self.session.scalars(query)
+        week_schedule = result.all()
+        return week_schedule
+    
+    
+    async def get_all_lessons(self) -> list[Schedule]:
+        query = (
+            self.basic_selection()
+            .order_by(Schedule.class_id, Schedule.day)
+        )
+        result = await self.session.scalars(query)
+        all_schedules = result.all()
+        return all_schedules
